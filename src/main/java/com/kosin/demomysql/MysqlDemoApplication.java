e00cb2888e0de1eabe196d623b6a6923b1403445
Patrick Kosin <patrick.kosin@adviqo.com>
2018-10-15 18:26:43 +0200

    init

---
+++ src/main/java/com/kosin/demomysql/MysqlDemoApplication.java
@@ -0,0 +1,12 @@
+package com.kosin.demomysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysqlDemoApplication.class, args);
+	}
+}
